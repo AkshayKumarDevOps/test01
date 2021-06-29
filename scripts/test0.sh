7272c7564c350bbfda3fd82f573c0465ff83c264
AkshayKumarDevOps <86013162+AkshayKumarDevOps@users.noreply.github.com>
2021-06-28 23:37:04 -0400

    Update test0.sh

--- scripts/test0.sh
+++ scripts/test0.sh
@@ -1,5 +1,5 @@
 source /home/ec2-user/.bash_profile
 #!/bin/bash
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
 npm install
 npm start
